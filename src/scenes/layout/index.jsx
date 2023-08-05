d0544d8bf2605e40ddd7e9d46cdb12c356ef3f61
Tahir Anjum <imtahiranjum@gmail.com>
2023-08-05 22:10:05 +0500

    sidebar and navbar updated

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -3,15 +3,31 @@ import { Box, useMediaQuery } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../../components/Navbar';
+import { useGetUserQuery } from 'state/api';
+import { useSelector } from 'react-redux';
+import Sidebar from 'components/Sidebar';
 
 
 const Layout = () => {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state)=> state.global.userId);
+  const { data } = useGetUserQuery(userId);
+  console.log("🚀 ~ file: index.jsx:19 ~ Layout ~ data:", data)
   
   return (
     <Box display={isNonMobile ? 'flex'  : 'block'} width="100%" height="100%">
+      <Sidebar
+        isNonMobile = {isNonMobile}
+        drawerWidth = "250px"
+        isSidebarOpen = {isSidebarOpen}
+        setIsSidebarOpen = {setIsSidebarOpen}
+      />
       <Box flexGrow={1}>
-        <Navbar />
+        <Navbar 
+          isSidebarOpen = {isSidebarOpen}
+          setIsSidebarOpen = {setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>
