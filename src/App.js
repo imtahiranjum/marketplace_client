c82c3e576c648e4ff5719e8db09e37047cfebe8e
Tahir Anjum <imtahiranjum@gmail.com>
2023-08-22 23:53:45 +0500

    added signin, signup routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import Layout from './scenes/layout';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { lightTheme } from './theme';
+import { lightTheme, themeSettings } from './theme';
 import Products from './scenes/products';
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
+import ProductDetails from 'scenes/productDetails';
+import Layout from 'scenes/layout';
+import SignIn from 'scenes/signin';
+import SignUp from 'scenes/signup';
 
   function App() {
+    const mode = useSelector((state) => state.global.mode)
     return (
     <div className="App">
       <BrowserRouter>
-        <ThemeProvider theme={lightTheme}>
+        <ThemeProvider theme={themeSettings}>
           <CssBaseline />
           <Routes>
             <Route element={<Layout />}>
-              <Route path='/' element = {<Navigate to="/home" replace />} />
-              <Route path='/products' element = {Products} />
+              <Route path='/' element = {<Navigate to="/products" replace />} />
+              <Route path='/products' element = {<Products/>} />
+              <Route path='/signin' element = {<SignIn/>} />
+              <Route path='/signup' element = {<SignUp/>} />
             </Route>
           </Routes>
         </ThemeProvider>
