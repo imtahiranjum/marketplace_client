3f1e1fc71d555680091a4c557b53046442dd5de2
Tahir Anjum <imtahiranjum@gmail.com>
2023-09-03 23:49:04 +0500

    redux toolkit settings

--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import globalReducer from "state"
+import { Provider } from 'react-redux';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { api } from 'state/api';
+import { ThemeProvider } from '@mui/material';
+import { themeSettings } from 'theme';
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    [api.reducerPath]: api.reducer, 
+  },
+  middleware: (getDefault)=> getDefault().concat(api.middleware)
+});
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={themeSettings}>
+
+      <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
+
