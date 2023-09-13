a2013103ed255c1057ff45c4d0ae9ee33420ba49
tahir_anjum <imtahiranjum@gmail.com>
2023-09-13 23:56:48 +0500

    Logos updated

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -4,6 +4,8 @@ import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Container from '@mui/material/Container';
 import { TextField, Typography } from '@mui/material';
+import facebookLogo from "../assets/facebook.png"
+import instaLogo from "../assets/insta.png"
 // import Typography from '../components/Typography';
 // import TextField from '../components/TextField';
 
@@ -11,8 +13,8 @@ function Copyright() {
   return (
     <React.Fragment>
       {'© '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="/">
+        eFarm
       </Link>{' '}
       {new Date().getFullYear()}
     </React.Fragment>
@@ -20,15 +22,12 @@ function Copyright() {
 }
 
 const iconStyle = {
-  width: 48,
-  height: 48,
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  backgroundColor: 'warning.main',
   mr: 1,
   '&:hover': {
-    bgcolor: 'warning.dark',
+    // bgcolor: 'success.dark',
   },
 };
 
@@ -37,18 +36,15 @@ const LANGUAGES = [
     code: 'en-US',
     name: 'English',
   },
-  {
-    code: 'fr-FR',
-    name: 'Français',
-  },
 ];
 
 export default function AppFooter() {
   return (
     <Typography
-      component="footer"
-      sx={{ display: 'flex', bgcolor: 'secondary.light' }}
+    component="footer"
+    sx={{ display: 'flex', bgcolor: 'secondary.light' }}
     >
+      <br/>
       <Container sx={{ my: 8, display: 'flex' }}>
         <Grid container spacing={5}>
           <Grid item xs={6} sm={4} md={3}>
@@ -60,16 +56,21 @@ export default function AppFooter() {
               sx={{ height: 120 }}
             >
               <Grid item sx={{ display: 'flex' }}>
-                <Box component="a" href="https://mui.com/" sx={iconStyle}>
+                <Box component="a" href="https://facebook.com/imtahiranjum" sx={iconStyle}
+                >
                   <img
-                    src="/static/themes/onepirate/appFooterFacebook.png"
-                    alt="Facebook"
+                    width= "48"
+                    height= "48"              
+                    src={facebookLogo}
+                    alt="Facebook Logo"
                   />
                 </Box>
-                <Box component="a" href="https://twitter.com/MUI_hq" sx={iconStyle}>
+                <Box component="a" href="https://instagram.com/tahir_anjum" sx={iconStyle} > 
                   <img
-                    src="/static/themes/onepirate/appFooterTwitter.png"
-                    alt="Twitter"
+                    width= "48"
+                    height= "48"
+                    src={instaLogo}
+                    alt="Instagram Logo"
                   />
                 </Box>
               </Grid>
@@ -84,10 +85,10 @@ export default function AppFooter() {
             </Typography>
             <Box component="ul" sx={{ m: 0, listStyle: 'none', p: 0 }}>
               <Box component="li" sx={{ py: 0.5 }}>
-                <Link href="/premium-themes/onepirate/terms/">Terms</Link>
+                <Link href="/">Terms</Link>
               </Box>
               <Box component="li" sx={{ py: 0.5 }}>
-                <Link href="/premium-themes/onepirate/privacy/">Privacy</Link>
+                <Link href="/">Privacy</Link>
               </Box>
             </Box>
           </Grid>
@@ -113,23 +114,7 @@ export default function AppFooter() {
           </Grid>
           <Grid item>
             <Typography variant="caption">
-              {'Icons made by '}
-              <Link href="https://www.freepik.com" rel="sponsored" title="Freepik">
-                Freepik
-              </Link>
-              {' from '}
-              <Link href="https://www.flaticon.com" rel="sponsored" title="Flaticon">
-                www.flaticon.com
-              </Link>
-              {' is licensed by '}
-              <Link
-                href="https://creativecommons.org/licenses/by/3.0/"
-                title="Creative Commons BY 3.0"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                CC 3.0 BY
-              </Link>
+              {'All Rights Reserved © 2023'}
             </Typography>
           </Grid>
         </Grid>
