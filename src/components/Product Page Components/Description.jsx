090b4832c60a694122239e097aef84a9206fecd8
tahir_anjum <imtahiranjum@gmail.com>
2023-09-13 23:57:51 +0500

    Added components for detail page

---
+++ src/components/Product Page Components/Description.jsx
@@ -0,0 +1,48 @@
+import { Box } from "@mui/material";
+import Typography from "components/Typography";
+import React, { useEffect, useState } from "react";
+import { useGetSpecificOnSaleCattleQuery } from "state/api";
+import CartIcon from "./Icons/CartIcon";
+
+const Description = ({
+  _id,
+  title,
+  image,
+  description,
+  price,
+  category,
+  location,
+  contact,
+  cattle_info,
+  questions,
+}) => {
+  const [clickedBuyButton, setClickedBuyButton] = useState(false);
+
+  return (
+    <Box className="description">
+      <Typography className="pre">{category}</Typography>
+    <h1>{title}</h1>
+      <p className="desc">
+        {description}
+      </p>
+      <div className="price">
+        <div className="main-tag">
+          <p>{price}</p>
+        </div>
+      </div>
+      <div className="buttons">
+      <button
+          className="add-to-cart"
+          onClick={() => {
+            setClickedBuyButton(true);
+          }}
+        >
+          <CartIcon />
+          add to cart
+        </button>
+      </div>
+    </Box>
+  );
+};
+
+export default Description;
