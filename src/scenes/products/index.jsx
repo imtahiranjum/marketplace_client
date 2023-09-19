f732d055fe98dd15a3ebcadf74b8c55f4ad5660a
tahir_anjum <imtahiranjum@gmail.com>
2023-09-19 22:45:57 +0500

    Added navigation for the product details page

--- src/scenes/products/index.jsx
+++ src/scenes/products/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Card,
@@ -6,17 +6,26 @@ import {
   CardContent,
   Collapse,
   Button,
-  Typography,
   Rating,
   useTheme,
   useMediaQuery,
+  CardMedia,
+  CardActionArea,
 } from "@mui/material";
 import Header from "../../components/Header";
-import { useGetProductQuery } from "state/api";
+import { useGetOnSaleCattleQuery } from "state/api";
+import profileImage from "assets/profile.jpeg";
+import { Image } from "@mui/icons-material";
+import Questions from "components/Questions";
+import Slider from "components/Slider";
+import Typography from "components/Typography";
+import { useNavigate } from "react-router-dom";
+// import BannerSlide from "components/BannerSlide";
 
-const Product = ({
+const OnSaleCattle = ({
   _id,
   title,
+  image,
   description,
   price,
   category,
@@ -25,41 +34,102 @@ const Product = ({
   cattle_info,
   questions,
 }) => {
+  const navigate = useNavigate();
+  const propsToPass = {
+    _id,
+    title,
+    image,
+    description,
+    price,
+    category,
+    location,
+    contact,
+    cattle_info,
+    questions,
+  };
+  const navigateToDetailsPage = () => {
+    navigate('/productdetails', { replace: true, state: {propsToPass} });
+  };
   const theme = useTheme();
   const [isExpanded, setIsExpanded] = useState(false);
+  const [buttonName, setButtonName] = useState("View More")
+  const handleClick = () => {
+    navigateToDetailsPage();
+  }
+
+  const handleButtonName = () => {
+    if (isExpanded){
+      setButtonName("View Less")
+    }
+    else {
+      setButtonName("View More") 
+    }
+  } 
+  
+  useEffect(() => {
+    handleButtonName()}
+    , [isExpanded]);
+  
+    // useEffect(() => {
+    //   handleButtonName()}
+    //   , [isExpanded]);
 
   return (
     <Card
       sx={{
-        backgroundImage: "none",
+        // backgroundImage: {profileImage}
         backgroundColor: theme.palette.secondary,
         borderRadius: "0.55rem",
+        borderColor: "darkgrey",
+        maxWidth: 345,
+             
       }}
-    >
+      >
+      <CardActions>
+        <Button onClick={handleClick}>
+
+      <CardMedia
+        sx={{ height: 200, width: 300, alignItems: "center", '&:hover': {
+          backgroundColor: 'primary.secondary',
+          opacity: [0.9, 0.8, 0.7],
+          justifyItems: "center"
+        },}}
+        image={image}
+        title={title}
+        />
+          </Button>
+      </CardActions>
       <CardContent>
         <Typography
-          sx={{ fontSize: 14 }}
+          sx={{ fontSize: 19 }}
           color={theme.palette.secondary}
           gutterBottom
         >
           {category}
         </Typography>
+        
         <Typography variant="h5" component="div">
           {title}
         </Typography>
         <Typography sx={{ mb: "1.5rem" }} color={theme.palette.secondary}>
-          ${Number(price).toFixed(2)}
+          Rs: {Number(price).toFixed(2)}
         </Typography>
 
         <Typography variant="body2">{description}</Typography>
       </CardContent>
       <CardActions>
         <Button
+        sx={{
+          '&:hover': {
+            backgroundColor: 'primary.secondary',
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
           variant="primary"
           size="small"
           onClick={() => setIsExpanded(!isExpanded)}
-        >
-          See More
+          >
+          {buttonName}
         </Button>
       </CardActions>
       <Collapse
@@ -67,13 +137,23 @@ const Product = ({
         timeout="auto"
         unmountOnExit
         sx={{
-          color: theme.palette.neutral[300],
+          color: theme.palette.secondary,
         }}
       >
         <CardContent>
-          <Typography>id: {_id}</Typography>
-          <Typography>
-            Questions: {questions}
+        <Typography>
+            Questions: {questions.map(
+              ({
+              description,
+              subject,
+
+            }) => ( <Questions
+              subject= {subject}
+              description= {description}
+            />
+            )
+            
+            )}
           </Typography>
         </CardContent>
       </Collapse>
@@ -81,56 +161,84 @@ const Product = ({
   );
 };
 
-const Products = () => {
-  const { data, isLoading } = useGetProductQuery();
-  const isNonMobile = useMediaQuery("(min-width: 1000px)");
+const AllOnSaleCattle = () => {
+  const { data, isLoading } = useGetOnSaleCattleQuery();
+  const isExtraLarge = useMediaQuery("(min-width: 1920px)");
+  const isLarge = useMediaQuery("(min-width: 1620px)");
+  const isNonMobile = useMediaQuery("(min-width: 1070px)");
+  const isTab = useMediaQuery("(min-width: 740px)");
+  const theme = useTheme()
+  const images = [
+          "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+          "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+          "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+          "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    ];
+  
 
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="ON SALE Cattle" subtitle="See your list of cattle in farm." />
-      {data || !isLoading ? (
+      {/* <Header sx={{
+         justifyContent: "left"
+      }} title="Cattle for Sale" subtitle="Currently available cattle for sale." /> */}
+      {/* <BannerSlide imageArray={images}/> */}
         <Box
-          mt="20px"
+
+          backgroundColor={"theme.palette.secondary"}
+          mt="23px"
+          color={theme.palette.primary}
           display="grid"
-          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+          position={"static"}
+          gridTemplateColumns={isExtraLarge? "repeat(5, minmax(0, 1fr))":isLarge? "repeat(4, minmax(0, 1fr))" :  isNonMobile ? "repeat(3, minmax(0, 1fr))" : isTab? "repeat(2, minmax(0, 1fr))": "repeat(1, minmax(0, 1fr))" }  
+          // : isTab? {gridTemplateColumns:"repeat(2, minmax(0, 1fr))"}:
+          //  {gridTemplateColumns:"repeat(1, minmax(0, 1fr))"}}
           justifyContent="space-between"
-          rowGap="20px"
-          columnGap="1.33%"
-          sx={{
-            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-          }}
+          rowGap="25px"
+          columnGap="1.0%"
+          justifyItems={"center"}
+          alignItems={"baseline"}
+          // sx={{
+          //   "& > div": { gridColumn: isNonMobile ? "span 1" : isTab? "span 2": "span 4" }, alignItems:"baseline"
+          // }}
         >
-          {data.map(
+        {data || !isLoading ? (
+          data.map(
             ({
               _id,
               title,
+              image,
               description,
               location,
               contact,
               category,
               cattle_info,
+              price,
               questions,
             }) => (
-              <Product
+              <OnSaleCattle
                 key={_id}
                 _id={_id}
                 title={title}
+                image={profileImage}
                 description={description}
                 category={category}
                 cattle_info={cattle_info}
                 contact={contact}
                 questions={questions}
                 location={location}
+                price={price}
 
               />
             )
-          )}
-        </Box>
+          )
       ) : (
-        <>Loading...</>
+        "Loading..."
       )}
+          
+        </Box>
+        
     </Box>
   );
 };
 
-export default Products;
+export default AllOnSaleCattle;
