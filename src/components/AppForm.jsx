55497ffcef68e674554aa0bef8ac34fdf84ce7c5
tahir_anjum <imtahiranjum@gmail.com>
2023-09-25 23:16:53 +0500

    Tweaked Appform for better look in forms

--- src/components/AppForm.jsx
+++ src/components/AppForm.jsx
@@ -1,28 +1,39 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Paper from './Paper';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import backgroundImage from "../assets/appCurvyLines.png";
+import { Card } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { Link } from "react-router-dom";
 
 function AppForm(props) {
   const { children } = props;
+  const theme = useTheme();
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        backgroundImage: 'url(/static/onepirate/appCurvyLines.png)',
-        backgroundRepeat: 'no-repeat',
+        display: "flex",
+        backgroundImage: { backgroundImage },
+        backgroundRepeat: "no-repeat",
       }}
     >
       <Container maxWidth="sm">
-        <Box sx={{ mt: 7, mb: 12 }}>
-          <Paper
-            background="light"
-            sx={{ py: { xs: 4, md: 8 }, px: { xs: 3, md: 6 } }}
+        <Box sx={{ mt: 10, mb: 16 }}>
+          <Card
+            background={theme.palette.secondary}
+            sx={{
+              py: { xs: 3, md: 6 },
+              px: { xs: 2, md: 5 },
+              backgroundColor: theme.palette.secondary,
+              borderRadius: "0.55rem",
+              borderWidth: "0.55rem",
+              borderColor: "darkgrey",
+            }}
           >
             {children}
-          </Paper>
+          </Card>
         </Box>
       </Container>
     </Box>
