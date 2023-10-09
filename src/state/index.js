d5b0f968759333d349197881070c7201f71c69bf
tahir_anjum <imtahiranjum@gmail.com>
2023-10-09 23:10:29 +0500

    Added reducer function and initial state

--- src/state/index.js
+++ src/state/index.js
@@ -1,19 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { useGetUserIdQuery } from "./api";
 
 const initialState = {
-    // mode: "dark",
+  userId: "",
+  userEmail: "",
+  // mode: "dark",
 };
 
 export const globalSlice = createSlice({
-    name: "global",
-    initialState,
-    reducers: {
-        setMode: (state) => {
-            // state.mode = state.mode === 'light' ? 'dark' : 'light';
-        }
-    }
-})
+  name: "global",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      // state.mode = state.mode === 'light' ? 'dark' : 'light';
+    },
+    setUserId: (state, userId) => {
+      console.log(state.userId);
+      state.userId = userId.payload;
+      console.log(state.userId);
+    },
+    setUserEmail: (state, userEmail) => {
+      console.log(state.userEmail);
+      state.userEmail = userEmail.payload;
+      console.log(state.userEmail);
+      state.userId = useGetUserIdQuery(userEmail);
+    },
+  },
+});
 
 // export const {setMode} = globalSlice.actions;
-
+export const { setUserEmail, setUserId } = globalSlice.actions;
 export default globalSlice.reducer;
