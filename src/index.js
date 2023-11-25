c8c963e0ccaa1069707d256f1be81acad283519b
Tahir Anjum <imtahiranjum@gmail.com>
2023-11-25 23:29:44 +0500

    Refactor index.js file and add Redux persist

--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { configureStore } from '@reduxjs/toolkit';
-import globalReducer from "state"
-import { Provider } from 'react-redux';
-import { setupListeners } from '@reduxjs/toolkit/dist/query';
-import { api } from 'state/api';
-import { ThemeProvider } from '@mui/material';
-import { themeSettings } from 'theme';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { api } from "state/api";
+import { ThemeProvider } from "@mui/material";
+import { themeSettings } from "theme";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, globalReducer);
 
 const store = configureStore({
   reducer: {
-    global: globalReducer,
-    [api.reducerPath]: api.reducer, 
+    global: persistedReducer,
+    [api.reducerPath]: api.reducer,
   },
-  middleware: (getDefault)=> getDefault().concat(api.middleware)
+  middleware: (getDefault) => getDefault().concat(api.middleware), thunk
 });
+
+
+const persistor=persistStore(store)
+
 setupListeners(store.dispatch);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={themeSettings}>
-
-      <App />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={themeSettings}>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
