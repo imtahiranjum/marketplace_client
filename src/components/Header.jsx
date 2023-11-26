f229fa851c63da8ecdf4da3daa31c72826c524ba
Tahir Anjum <imtahiranjum@gmail.com>
2023-11-26 23:43:01 +0500

    Update Header component styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
-import { Typography, Box, useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import React from "react";
+import Typography from "./Typography";
 
 const Header = ({ title, subtitle }) => {
   const theme = useTheme();
   return (
-    <Box>
+    <Box sx={{
+      justifyContent: "left"
+    }}>
       <Typography
-        variant="h2"
+        variant="h3"
         color={theme.palette.secondary}
         fontWeight="bold"
-        sx={{ mb: "5px" }}
+        sx={{ mb: "5px", justifyContent: "left"}}
       >
         {title}
       </Typography>
