8d4440f93664dc952ea20dd4444e8d33acccecdf
Tahir Anjum <imtahiranjum@gmail.com>
2023-12-11 21:17:19 +0500

    Add useEffect and useState hooks to Chat2 component

--- src/scenes/chat2/index.jsx
+++ src/scenes/chat2/index.jsx
@@ -1,36 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar, Divider, Fab, Grid, List, ListItem, ListItemIcon, ListItemText, Paper, TextField } from '@mui/material';
 import Typography from 'components/Typography';
 import { Send } from '@mui/icons-material';
-
-// const useStyles = makeStyles({
-//   table: {
-//     minWidth: 650,
-//   },
-//   chatSection: {
-//     width: '100%',
-//     height: '80vh'
-//   },
-//   headBG: {
-//       backgroundColor: '#e0e0e0'
-//   },
-//   borderRight500: {
-//       borderRight: '1px solid #e0e0e0'
-//   },
-//   messageArea: {
-//     height: '70vh',
-//     overflowY: 'auto'
-//   }
-// });
+import { useSelector } from 'react-redux';
+import { useGetUserByEmailQuery } from 'state/api';
 
 const Chat2 = () => {
-//   const classes = useStyles();
+    const userEmail = useSelector((state) => state.global.userEmail);
+    const {data, isLoading, isSuccess} = useGetUserByEmailQuery(userEmail);
+    const [userId, setUserId] = useState("");
+
+    useEffect(() => {
+        if (isSuccess && !isLoading) {
+            setUserId(data.id);
+        }
+    }, [isSuccess]);
 
   return (
       <div>
         <Grid container>
             <Grid item xs={12} >
-                <Typography variant="h5" className="header-message">Chat</Typography>
+                <Typography
+                sx={{
+                    fontWeight: 600,
+                    marginTop: 2,
+                    marginBottom: 2,
+                    mx: '1rem',
+                }}
+                variant="h5" className="header-message">Chat</Typography>
             </Grid>
         </Grid>
         <Grid container component={Paper}>
@@ -38,15 +35,15 @@ const Chat2 = () => {
                 <List>
                     <ListItem key="RemySharp">
                         <ListItemIcon>
-                        <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                        <Avatar alt="" src={data && data.profile? data.profile.profile_image: "Loading"} />
                         </ListItemIcon>
-                        <ListItemText primary="John Wick"></ListItemText>
+                        <ListItemText primary={data? data.full_name: "Loading"}></ListItemText>
                     </ListItem>
                 </List>
                 <Divider />
-                <Grid item xs={12} style={{padding: '10px'}}>
+                {/* <Grid item xs={12} style={{padding: '10px'}}>
                     <TextField id="outlined-basic-email" label="Search" variant="outlined" fullWidth />
-                </Grid>
+                </Grid> */}
                 <Divider />
                 <List>
                     <ListItem  key="RemySharp">
